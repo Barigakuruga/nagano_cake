09418616c96222035e8bf1a8463979691194c0c5
EC2 Default User <ec2-user@ip-172-31-31-120.ap-northeast-1.compute.internal>
2023-02-12 08:16:28 +0000

    [save]中間保存

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,20 @@
 class Admin::ItemsController < ApplicationController
   def index
-    
+    @items = Item.all
   end
 
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_items_path(@item)
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
